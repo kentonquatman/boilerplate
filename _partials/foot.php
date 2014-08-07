94ab98ce2f58b69c635c32b8a38157e673dd6fed
Kenton Quatman <kentonquatman@gmail.com>
2014-08-07 13:02:40 -0400

    Updated asset and include paths

--- _partials/foot.php
+++ _partials/foot.php
@@ -1,11 +1,11 @@
 <script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
-<script>window.jQuery || document.write('<script src="/assets/js/jquery.js"><\/script>')</script>
-<script src="/assets/js/scripts.foot.min.js"></script>
+<script>window.jQuery || document.write('<script src="_assets/js/jquery.js"><\/script>')</script>
+<script src="_assets/js/scripts.foot.min.js"></script>
 <!--[if (lt IE 9) & (!IEMobile)]>
-  <script src="/assets/js/scripts.ie8.min.js"></script>
+  <script src="_assets/js/scripts.ie8.min.js"></script>
 <![endif]-->
 <!--[if (lt IE 8) & (!IEMobile)]>
-  <script src="/assets/js/imgsizer.js"></script>
+  <script src="_assets/js/imgsizer.js"></script>
   <script>
     addLoadEvent(function() {
       if (document.getElementById && document.getElementsByTagName) {
--- _partials/head.php
+++ _partials/head.php
@@ -27,14 +27,14 @@
 
   <meta property="og:title" content="Page Title Repeated Here">
   <meta property="og:description" content="Page Description Repeated Here">
-  <meta property="og:image" content="/assets/img/fav/open-graph.png">
+  <meta property="og:image" content="_assets/img/fav/open-graph.png">
   <meta property="og:url" content="<?php echo $siteUrl; ?>">
 
   <meta name="HandheldFriendly" content="True">
   <meta name="MobileOptimized" content="320">
   <meta name="viewport" content="width=device-width, initial-scale=1">
 
-  <link type="text/css" rel="stylesheet" href="/assets/css/style.css">
+  <link type="text/css" rel="stylesheet" href="_assets/css/style.css">
 
   <script>
     var b = document.documentElement;
@@ -42,18 +42,18 @@
     b.setAttribute('data-platform', navigator.platform );
   </script>
 
-  <script src="/assets/js/scripts.head.min.js"></script>
+  <script src="_assets/js/scripts.head.min.js"></script>
 
-  <link rel="apple-touch-icon-precomposed" href="/assets/fav/apple-touch-icon-precomposed.png">
-  <link rel="apple-touch-icon-precomposed" sizes="57x57" href="/assets/fav/apple-touch-icon-57x57-precomposed.png">
-  <link rel="apple-touch-icon-precomposed" sizes="60x60" href="/assets/fav/apple-touch-icon-60x60-precomposed.png">
-  <link rel="apple-touch-icon-precomposed" sizes="72x72" href="/assets/fav/apple-touch-icon-72x72-precomposed.png">
-  <link rel="apple-touch-icon-precomposed" sizes="76x76" href="/assets/fav/apple-touch-icon-76x76-precomposed.png">
-  <link rel="apple-touch-icon-precomposed" sizes="114x114" href="/assets/fav/apple-touch-icon-114x114-precomposed.png">
-  <link rel="apple-touch-icon-precomposed" sizes="120x120" href="/assets/fav/apple-touch-icon-120x120-precomposed.png">
-  <link rel="apple-touch-icon-precomposed" sizes="144x144" href="/assets/fav/apple-touch-icon-144x144-precomposed.png">
-  <link rel="apple-touch-icon-precomposed" sizes="152x152" href="/assets/fav/apple-touch-icon-152x152-precomposed.png">
-  <link rel="shortcut icon" href="/assets/fav/favicon.ico">
+  <link rel="apple-touch-icon-precomposed" href="_assets/fav/apple-touch-icon-precomposed.png">
+  <link rel="apple-touch-icon-precomposed" sizes="57x57" href="_assets/fav/apple-touch-icon-57x57-precomposed.png">
+  <link rel="apple-touch-icon-precomposed" sizes="60x60" href="_assets/fav/apple-touch-icon-60x60-precomposed.png">
+  <link rel="apple-touch-icon-precomposed" sizes="72x72" href="_assets/fav/apple-touch-icon-72x72-precomposed.png">
+  <link rel="apple-touch-icon-precomposed" sizes="76x76" href="_assets/fav/apple-touch-icon-76x76-precomposed.png">
+  <link rel="apple-touch-icon-precomposed" sizes="114x114" href="_assets/fav/apple-touch-icon-114x114-precomposed.png">
+  <link rel="apple-touch-icon-precomposed" sizes="120x120" href="_assets/fav/apple-touch-icon-120x120-precomposed.png">
+  <link rel="apple-touch-icon-precomposed" sizes="144x144" href="_assets/fav/apple-touch-icon-144x144-precomposed.png">
+  <link rel="apple-touch-icon-precomposed" sizes="152x152" href="_assets/fav/apple-touch-icon-152x152-precomposed.png">
+  <link rel="shortcut icon" href="_assets/fav/favicon.ico">
 
   <link rel="canonical" content="<?php echo $siteUrl; ?>">
 
